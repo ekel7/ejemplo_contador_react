54a223137b0ca6dfb91bd6231e736c9b9860de0e
Carla Vallejos <vallejoscarla96@gmail.com>
2022-12-01 10:42:59 -0300

    segundo commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
      <Navbar gato={array}  perro="perritos"  ave="avecita"  venado="venadito"/>
-     <ItemListContainer bienvenida="Holaaa! Bienvenida!"/>
+     <ItemListContainer bienvenida="Holaaa! Bienvenido!"/>
     </div>
 
   );
